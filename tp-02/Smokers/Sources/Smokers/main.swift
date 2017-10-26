a5504849d8befcded37d1ddd78c5d87097527c52
TommasoPeletta <tommaso.peletta@etu.unige.ch>
2017-10-26 17:36:18 +0200

    add tp-02

--- tp-02/Smokers/Sources/Smokers/main.swift
+++ tp-02/Smokers/Sources/Smokers/main.swift
@@ -1,5 +1,6 @@
 import PetriKit
 import SmokersLib
+import Foundation
 
 // Instantiate the model.
 let model = createModel()
@@ -19,10 +20,91 @@ else {
     fatalError("invalid model")
 }
 
+try model.saveAsDot(to: URL(fileURLWithPath: "model.dot"), withMarking: [r: 1, w1: 1, w2: 1, w3:1])
 // Create the initial marking.
 let initialMarking: PTMarking = [r: 1, p: 0, t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2: 0, w3: 1, s3: 0]
 
-// Create the marking graph (if possible).
+// function implementè en classe pour compter les etats en parcourant le markinggraph
+public func countNodes(markingGraph: MarkingGraph)-> Int {
+  var seen = [markingGraph]
+  var toVisit = [markingGraph]
+  while let current = toVisit.popLast(){
+    for(_,successors) in current.successors {
+      if !seen.contains(where: {$0 === successors}){
+        seen.append(successors)
+        toVisit.append(successors)
+      }
+    }
+  }
+  return seen.count
+}
+
+/* *******************************************************************************************
+Cette focntion permet de savoir si il est possible que deux fumeurs fumes aux meme temps.
+l'ideè c'est de parcourir notre markingraph (1)
+est tester à chaque iteration si il y a deux fumeurs aux meme temps (2).
+la fonction return 1 si il y a deux fumeurs aux meme temps (3)
+la fonction return 0 si il est pas possible que deux fumeurs fume aux meme temps (4)
+******************************************************************************************** */
+public func fumeurs_aux_meme_temps(markingGraph: MarkingGraph) -> Int {
+  var seen = [markingGraph]
+  var toVisit = [markingGraph]
+  while let current = toVisit.popLast(){ //(1)
+    for(_,successors) in current.successors {//(1)
+      if !seen.contains(where: {$0 === successors}){//(1)
+        seen.append(successors)//(1)
+        toVisit.append(successors)//(1)
+        let a = successors.marking[s1]//(1)
+        let b = successors.marking[s2]//(1)
+        let c = successors.marking[s3]//(1)
+        if (a == 1 && b == 1) || (a == 1 && c == 1) || (b == 1 && c == 1) {//(2)
+          return 1 //(3)
+        }
+      }
+    }
+  }
+      return 0//(4)
+    }
+/***********************************************************************************************
+Cette fonction permet de savoir si il est possible que il y a deux ingredient ègale sur la table.
+l'ideè c'est de parcourir notre markingraph (1)
+est tester à chaque iteration si il y a deux ingredient ègale sur la table (2).
+la fonction return 1 si il y a deux ingredient ègale sur la table (3)
+la fonction return 0 si il n'y a pas deux ingredient ègale sur la table (4)
+************************************************************************************************/
+  public func two_ingredient(markingGraph: MarkingGraph) -> Int {
+    var seen = [markingGraph]
+    var toVisit = [markingGraph]
+    while let current = toVisit.popLast(){ //(1)
+      for(_,successors) in current.successors { //(1)
+        if !seen.contains(where: {$0 === successors}){ //(1)
+          seen.append(successors) //(1)
+          toVisit.append(successors) //(1)
+          let a = successors.marking[p]
+          let b = successors.marking[t]
+          let c = successors.marking[m]
+          if (a == 2 || b == 2 || c == 2) { //(2)
+            return 1 //(3)
+          }
+        }
+      }
+    }
+        return 0 //(4)
+  }
+
+  // Main on lance les fonction pour notre resaux
 if let markingGraph = model.markingGraph(from: initialMarking) {
-    // Write here the code necessary to answer questions of Exercise 4.
+  let Ex_4_a = countNodes(markingGraph: markingGraph)
+  print("nombres d'etat:")
+  print(Ex_4_a)
+  if 1 == fumeurs_aux_meme_temps(markingGraph: markingGraph) {
+    print("deux fumeurs peuvent fumer aux meme temps")
+  } else {
+    print("deux fumeurs ne peuvent pas fumer aux meme temps")
+  }
+  if 1 == two_ingredient(markingGraph: markingGraph) {
+    print("il est possible de avoir deux fois le meme ingredient sur la table")
+  } else {
+    print("il n'est pas possible de avoire le meme ingredient sur la table")
+}
 }
--- tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -15,8 +15,28 @@ public class MarkingGraph {
 public extension PTNet {
 
     public func markingGraph(from marking: PTMarking) -> MarkingGraph? {
-        // Write here the implementation of the marking graph generation.
-        return nil
+      let m0 = MarkingGraph(marking: marking) //MarkingGraph avec marquage initial inisialise
+      var m2 = [MarkingGraph]() //liste vide avec marquage traiter
+      var m1 = [m0] //liste avec marquage à traiter
+      while m1.last != nil { //si la liste avec les marquage à traiter est vide
+        let marking_courant = m1.popLast() //on definit le marquage à traiter comem le dernier element de m1
+        m2.append(marking_courant!) //on passe le marquage courant comme traiter
+        let marquage_courant = marking_courant!.marking
+        for i in self.transitions { //pour toutes les transitions dans notre model
+        if i.fire(from: marquage_courant) != nil { // si il est possible de tirer la transision i depuis le marquage courant
+          let m_next = i.fire(from: marquage_courant)
+          if m2.contains(where: {$0.marking == m_next!}) { // si le marquage succesive est deja dans la liste des marquage a traiter
+            marking_courant!.successors[i] = m2.first(where: {$0.marking == m_next!}) //le marquage succesive est egale aux markingGraph deja contenu dans m2
+          } else if m1.contains(where: {$0.marking == m_next!}) {// si le marquage succesive est deja dans m1
+            marking_courant!.successors[i] = m1.first(where: {$0.marking == m_next!}) } //le marquage succesive est egale aux marquage contenu dans m1
+            else { // si non
+            let n = MarkingGraph(marking : m_next!)
+            marking_courant!.successors[i] = n //le marquage succesive et le resultat du i.fire
+            m1.append(n) //on mais le marquage trouver comme marquage à traiter
+          }
+        }
+      }
     }
-
+    return m0
+  }
 }
